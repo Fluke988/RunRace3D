ca9ab1872b482d79abb2e45e64f841b5cfbf32bd
Fluke988 <jagtaphrushi@gmail.com>
2022-02-17 09:58:15 +0530

    Commit3
    - Configured Char controller
    - Added Player Controller script

--- RunRace/Assets/Scripts/PlayerController.cs
+++ RunRace/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    CharacterController characterController;
+    private Vector3 motion;
+    public float speed = 3.0f;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        motion = new Vector3(0, 0, 0);
+        motion = transform.forward;
+        motion.Normalize();
+
+        motion = motion * speed * Time.deltaTime;
+        characterController.Move(motion);
     }
 }
